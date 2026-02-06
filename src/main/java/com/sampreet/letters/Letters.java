c909ab64c349e7e202987bb76fe421b56a5525c7
Sampreet Singh <preet.samsam+github@gmail.com>
2026-02-07 02:05:54 +0530

    setup basic plugin skeleton

---
+++ src/main/java/com/sampreet/letters/Letters.java
@@ -0,0 +1,17 @@
+package com.sampreet.letters;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Letters extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
